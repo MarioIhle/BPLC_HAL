b70de02e5a8728739921716411d03138bb0771fd
Mario Ihle <mario.ihle@dormkaba.com>
2023-11-14 10:55:21 +0100

    Funktionsumfang erweitert

--- BPLC_LOG/BPLC_LOG.h
+++ BPLC_LOG/BPLC_LOG.h
@@ -7,17 +7,14 @@
 #include "BPLC_ERRORS.h"
 
 
-
 typedef enum
 {
-    BPLC_EVENT_LOG__DIN11_CARD__PORT_CHANGED,
-    BPLC_EVENT_LOG__REL11_CARD__PORT_WRITE_LOW, 
-    BPLC_EVENT_LOG__REL11_CARD__PORT_WRITE_HIGH,
+    BPLC_EVENT_LOG__DIN11_CARD_PORT_CHANGED,
+    BPLC_EVENT_LOG__REL11_CARD_PORT_WRITE_LOW, 
+    BPLC_EVENT_LOG__REL11_CARD_PORT_WRITE_HIGH,
+    BPLC_EVENT_LOG__MAP_OBJECT_TO_REL11_CARD,
 
 }e_BPLC_EVENT_t;
-
-
-
 
 
 typedef struct 
@@ -28,8 +25,23 @@ typedef struct
 
 }s_BASE_LOG_t;
 
+#define MAX_EVENTS_PER_CYCLE 50
+
+class BPLC_log
+{
+    public:
+    BPLC_log();
+
+    void tick();
+    void logEvent(const e_BPLC_EVENT_t EVENT_TYPE, const uint8_t PAYLOAD, unsigned long TIMESTAMP);
+    void logEvent(const e_BPLC_EVENT_t EVENT_TYPE, const float   PAYLOAD, unsigned long TIMESTAMP);
+    void logEvent(const e_BPLC_EVENT_t EVENT_TYPE, const int16_t PAYLOAD, unsigned long TIMESTAMP);
 
 
+    private:
 
+    void            printEventBuffer();
+    s_BASE_LOG_t    eventBuffer[MAX_EVENTS_PER_CYCLE];
 
+};
 #endif
