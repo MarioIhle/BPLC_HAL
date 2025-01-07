55894f08d6e7512b6717113a8945737f8ce93951
Mario_PC_Home <mario-ihle@gmx.de>
2025-01-07 20:33:40 +0100

    code aufgräumt

--- 03_BPLC_ioBaseTypes/types/counter.h
+++ 03_BPLC_ioBaseTypes/types/counter.h
@@ -25,14 +25,21 @@ class counter: public IO_Interface
     }
     //Hal handling
     e_IO_TYPE_t         getIoType       (){return this->ioType;}
-    bool                newDataAvailable(){return false;}
-    u_HAL_DATA_t        halCallback     (u_HAL_DATA_t* P_DATA){if(P_DATA->digitalIoData.state == true && this->previousState == false){this->count++;} this->previousState = P_DATA->digitalIoData.state; return *P_DATA;}
-    
+    bool                newDataAvailable(){this->count++;return false;} //cout++ ist ein kleiner Hack um über die INT isr direkt zählen zu können 
+    u_HAL_DATA_t        halCallback     (u_HAL_DATA_t* P_DATA)
+    {
+        if(P_DATA->digitalIoData.state == true && this->previousState == false)
+        {
+            this->count++;
+        } 
+        this->previousState = P_DATA->digitalIoData.state; 
+        return *P_DATA;
+    }
+
 
     private:
     volatile uint64_t   count; 
     e_IO_TYPE_t         ioType;
     volatile bool       previousState;
 };
-
 #endif
--- 03_BPLC_ioBaseTypes/types/rpmSens.h
+++ 03_BPLC_ioBaseTypes/types/rpmSens.h
@@ -4,35 +4,70 @@
 #include "Arduino.h"
 #include "00_ioInterface.h"
 #include "SpecialFunctions.h"
-
-
-#define MAX_SAMPLES_UNTIL_CALCULATION   500
+#include "BPLC_logPrint.h"
 
 class rpmSensor: public IO_Interface
 {
     public:
-                        rpmSensor           (){this->ioType = IO_TYPE__RPM_SENS;}
-    void                begin               (const uint16_t PULSES_PER_REV = 1, const uint16_t SAMPLE_TIME = 500)
+    rpmSensor()
+    {
+        this->ioType                = IO_TYPE__RPM_SENS;       
+    }
+
+    void begin(const uint64_t PULSES_PER_REV = 1, const uint64_t SAMPLE_TIME = 500)
     {
         this->pulsesPerRevolution 	= PULSES_PER_REV;
         this->startTime 			= millis();
-        this->sampleCounter.resetCount();
-        this->to_rpmCalculation.setInterval(SAMPLE_TIME);
+        this->rpm                   = 0;
+        this->p_sampleCounter       = new counter;
+
+        this->p_sampleCounter->resetCount();
+        this->to_rpmCalculation.setInterval(SAMPLE_TIME);        
     }
-    uint16_t            getRPM              ()
+
+    void begin(counter* P_SAMPLE_COUNTER, const uint64_t PULSES_PER_REV = 1, const uint64_t SAMPLE_TIME = 500)
     {
-	    if(to_rpmCalculation.check())
+        this->p_sampleCounter       = P_SAMPLE_COUNTER;
+        this->pulsesPerRevolution 	= (PULSES_PER_REV * 2);   //wenn externer Counter verwendet wird, ist das wahrscheinlich direkt die INT_ISR, diese wird 2x pro periode getriggert, daher PULSES_PER_REV*2
+        this->startTime 			= millis();
+        this->rpm                   = 0;
+        
+        this->p_sampleCounter->resetCount();
+        this->to_rpmCalculation.setInterval(SAMPLE_TIME);        
+    }
+
+    uint64_t getRPM()
+    {
+        if(this->p_sampleCounter != nullptr)
         {
-            const double 	TIME_DELTA 		= millis()-this->startTime;
-            const double 	SAMPLES			= (double)this->sampleCounter.getCount();
-            const double 	REVOLUTIONS		= SAMPLES / this->pulsesPerRevolution;
-            const double	RPM 	 		= (REVOLUTIONS/TIME_DELTA) * 60000.00;
-            //Alle Werte zurück setzten, neue Messung starten
-            this->startTime = millis();		
-            this->rpm 		= (uint16_t)RPM;
-            this->sampleCounter.resetCount();
-            this->to_rpmCalculation.reset();//nach berechnung resetten, zwecks genauigkeit		
+            if(to_rpmCalculation.checkAndReset())
+            {
+                //Daten vorladen
+                const double OS_TIME                    = (double)millis();
+                const double SAMPLES			        = (double)this->p_sampleCounter->getCount();                 
+                const double START_TIME                 = (double)this->startTime;
+                const double TIME_DELTA 		        = (OS_TIME - START_TIME);
+                const double PULSES_PER_REVOLUTION      = (double)this->pulsesPerRevolution;                                           
+                //Drehzahl berechnen
+                const double REVOLUTIONS	            = (SAMPLES / PULSES_PER_REVOLUTION);
+                const double TIME_PER_REV               = (REVOLUTIONS/TIME_DELTA);
+                const double CALCULATED_RPM             = (TIME_PER_REV * 60000.00);
+                //Drehzahl glätten
+                const double RPM_OLD                    = (double)this->rpm;
+                this->rpm                               = (uint64_t)CALCULATED_RPM; //(CALCULATED_RPM * 0.25) + (RPM_OLD * 0.75);
+        
+                //Alle Werte zurück setzten, neue Messung starten 
+                this->startTime = millis();	
+                this->p_sampleCounter->resetCount();    	
+            }
         }
+        else
+        {
+            BPLC_logPrint log;
+            log.printResetReason("RPM_SENS_NOT_INITILIZED", __FILENAME__, __LINE__);
+            ESP.restart();
+        }
+	    
         return this->rpm;
     }
     
@@ -41,20 +76,30 @@ class rpmSensor: public IO_Interface
     bool                newDataAvailable    (){return false;}
     u_HAL_DATA_t        halCallback         (u_HAL_DATA_t* P_DATA = nullptr)
     {
-        this->sampleCounter.halCallback(P_DATA);
+        if(this->p_sampleCounter != nullptr)
+        {
+            this->p_sampleCounter->halCallback(P_DATA);
+        }
+        else
+        {
+            BPLC_logPrint log;
+            log.printResetReason("RPM_SENS_NOT_INITILIZED", __FILENAME__, __LINE__);
+            ESP.restart();
+        }
+        
         return *P_DATA;
     }
 
 
     private:     
-    e_IO_TYPE_t         ioType;
-    counter             sampleCounter;
-    unsigned long       startTime;
+    e_IO_TYPE_t ioType;
+    counter*    p_sampleCounter;
+    uint64_t    startTime;
 
-    uint16_t            rpm;
-    uint16_t            pulsesPerRevolution;
+    uint64_t    rpm;
+    uint64_t    pulsesPerRevolution;   
 
-    Timeout             to_rpmCalculation;
+    Timeout     to_rpmCalculation;
 };
 
 #endif
