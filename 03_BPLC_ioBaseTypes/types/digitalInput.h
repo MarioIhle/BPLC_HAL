f723811a8d952329c2629358f2fc6451a8d0d9b9
Mario_PC_Home <mario-ihle@gmx.de>
2025-01-11 20:32:46 +0100

    Warning message hinzugefügt

--- 03_BPLC_ioBaseTypes/types/digitalInput.h
+++ 03_BPLC_ioBaseTypes/types/digitalInput.h
@@ -4,13 +4,22 @@
 #include "Arduino.h"
 #include "digitalDebouncer.h"
 #include "types/00_ioInterface.h"
+#include "BPLC_logPrint.h"
 
-class digitalInput: public IO_Interface
+class digitalInput: public IO_Interface, private BPLC_logPrint
 {
     public:
     //init
                         digitalInput    (){this->ioType = IO_TYPE__DIGITAL_INPUT; this->state = false; this->previousState = false;}
-    void                setDebounceTime (const uint8_t LOW_TO_HIGH_MS, const uint8_t HIGH_TO_LOW_MS){this->debouncer.setDebounceTime(LOW_TO_HIGH_MS, HIGH_TO_LOW_MS);}
+    void                setDebounceTime (const uint8_t LOW_TO_HIGH_MS, const uint8_t HIGH_TO_LOW_MS)
+    {
+        this->debouncer.setDebounceTime(LOW_TO_HIGH_MS, HIGH_TO_LOW_MS);
+        
+        if(LOW_TO_HIGH_MS >= 10 || HIGH_TO_LOW_MS >= 10)
+        {
+            this->printLog("MAY_THE_ECM_CAN_NOT_DETECT_RISING_EDGE_ANYMORE", __FILENAME__, __LINE__);
+        }
+    }
     //Applikation
     bool                stateChanged    (){const bool STATE_CHANGED = (this->f_stateChanged == true); this->f_stateChanged = false; return STATE_CHANGED;}
     bool 	            high            (){return (this->state == true);}       //abwärtsKompatibel
@@ -19,7 +28,6 @@ class digitalInput: public IO_Interface
     bool	            islow           (){return (this->state == false);}      //abwärtsKompatibel
     bool 	            isHigh          (){return (this->state == true);}
     bool	            isLow           (){return (this->state == false);}
-
 	bool 	            risingEdge      ()
     {
         if(this->f_risingEdgeOccoured)
